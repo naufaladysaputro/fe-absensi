f1dcba68ff10d01d2b781fc53094d5d642922e4c
naufaladysaputro <157557765+naufaladysaputro@users.noreply.github.com>
2025-07-19 21:18:49 +0700

    update: api_url

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
 // export const API_URL = 'https://satisfactory-sonya-naufaladysaputro-f8d7a5ae.koyeb.app';
-export const API_URL = 'http://103.118.175.127:5000/';
+export const API_URL = 'https://be_naufal.1cloud.my.id/';
